7ed8b47e77b3b729c0d635d4ed1886efdd0171cb
Matan Nahmani <matanmatan999new@gmail.com>
2020-09-14 20:53:44 +0900

    save before devise controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  before_validation :checkphone
+  before_save :checkphone
   after_create { self.healthcheck = true }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
