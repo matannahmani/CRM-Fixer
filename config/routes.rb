40415354f1d2d4f3682da6c2b76d7b9b7ae3908d
Matan Nahmani <matanmatan999new@gmail.com>
2020-09-21 16:15:34 +0300

    FRONT ENDDD MASTER a lot of design implations

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,13 @@ Rails.application.routes.draw do
   resources :cities, path: "admin/cities/"
   resources :users, path: "admin/users/"
   get 'call/:id', to: 'calls#show', as: 'showcall'
+  get 'call/new', to: 'calls#new', as: 'newcall'
   devise_for :users, controllers: { registrations: "registrations" }
   # devise_for :users
   root to: 'pages#home'
   get '/calls/takecall/:id', to: 'calls#takecall', as: 'takecall'
   get '/mycard', to: 'pages#show', as: 'mypdf'
-  get '/testcard', to: 'pages#volcard'
+  # get '/testcard', to: 'pages#volcard' # PDF PREVIEW
   get '/admin', to: 'pages#adminpanel', as: 'adminpanel'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
